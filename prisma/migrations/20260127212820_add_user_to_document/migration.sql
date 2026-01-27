e42aa7bc736ebd1ddae871e5f207c7998ad364fe
Axl Tan <axllltan@gmail.com>
2026-01-27 13:46:47 -0800

    Updated schema and urls

---
+++ prisma/migrations/20260127212820_add_user_to_document/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Document` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Document" ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE INDEX "Document_userId_idx" ON "Document"("userId");
